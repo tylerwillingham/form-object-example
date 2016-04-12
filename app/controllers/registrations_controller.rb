f49e63753e57a39cd9d4edbc5f442d8c2f9544bf
Tyler Willingham <tylerwillingham@gmail.com>
2016-04-11 20:01:28 -0500

    Create functional registration form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,41 @@
+# TODO:
+# 1. Validate that `password` and `password_confirmation` match
+# 2. All attributes on User are required for the form
 class RegistrationsController < ApplicationController
   def new
+    @company  ||= Company.new.tap do |company|
+                    company.users.new
+                  end
   end
 
   def create
+    @company = Company.new company_params
+
+    if @company.save
+      redirect_to(
+        new_registration_path,
+        flash: { notice: 'Registered successfully!' }
+      )
+    else
+      render :new, flash: { error: 'Registration failed' }
+    end
+  end
+
+  private
+
+  def company_params
+    params
+      .require(:company)
+      .permit(
+        :name,
+        users_attributes: [
+          :first_name,
+          :last_name,
+          :email,
+          :username,
+          :password,
+          :password_confirmation
+        ]
+      )
   end
 end
