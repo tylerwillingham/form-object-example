db5c6a24d7befa9e87203a3489ff8fb6ffb309fc
Tyler Willingham <tylerwillingham@gmail.com>
2016-04-12 10:22:05 -0500

    Form object implementation

--- app/forms/registration_form.rb
+++ app/forms/registration_form.rb
@@ -11,12 +11,8 @@ class RegistrationForm
 
   validates :company_name, presence: true
   validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true
   validates :username, presence: true
   validates :password, presence: true
-  validates :password_confirmation, presence: true
-  validate :validate_password_and_confirmation_match
 
   def submit
     return false unless valid?
@@ -35,17 +31,5 @@ class RegistrationForm
 
   def to_partial_path
     'registrations/form'
-  end
-
-  private
-
-  def password_and_confirmation_match?
-    password == password_confirmation
-  end
-
-  def validate_password_and_confirmation_match
-    return if password_and_confirmation_match?
-
-    errors.add(:password, 'and confirmation do not match')
   end
 end
